b5d7833c80596ce656d7e0cff635ff6ff5fd301c
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2023-10-02 16:52:08 -0700

    updating logger

--- Sensor_Fault_Detection/logger.py
+++ Sensor_Fault_Detection/logger.py
@@ -6,11 +6,9 @@ from datetime import datetime
 LOG = "logs" 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path = os.path.join(os.getcwd(), LOG, LOG_FILE)
+os.makedirs(LOG, exist_ok=True)
 
-os.makedirs(logs_path, exist_ok=True)
-
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(LOG, LOG_FILE)
 
 logging.basicConfig(filename= LOG_FILE_PATH,
                     format= "[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
