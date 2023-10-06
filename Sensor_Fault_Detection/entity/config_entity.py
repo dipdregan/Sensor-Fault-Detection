a3d39eb72a55ec5e73bd7fd2e4936d9598ec10e8
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2023-10-05 17:18:40 -0700

    completed artifact, config for data transformation

--- Sensor_Fault_Detection/entity/config_entity.py
+++ Sensor_Fault_Detection/entity/config_entity.py
@@ -90,4 +90,19 @@ class DataValidationConfig:
         
 
         
+class DataTransformationConfig:
+    def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join(training_pipeline_config.artifact_dir,
+                                                         Training_pipeline.DATA_TRANSFORMATION_DIR_NAME
+                                                         )
+        self.transformed_train_file_path: str = os.path.join(self.data_transformation_dir,
+                                                             Training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+                                                             Training_pipeline.TRAINING_FILE_NAME.replace("csv", "npy"))
+                                                         
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,
+                                                             Training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+                                                             Training_pipeline.TESTING_FILE_NAME.replace("csv", "npy"))
         
+        self.transformed_object_file_path: str = os.path.join(self.data_transformation_dir,
+                                                              Training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+                                                              Training_pipeline.PREPROCESS_OBJECT_FILE_NAME)
