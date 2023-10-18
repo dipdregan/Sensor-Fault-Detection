3571ace08290bc4657bf6310476c0a6d677beceb
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2023-10-18 03:52:36 -0700

    adding model training constant file

--- Sensor_Fault_Detection/constants/Training_pipeline/__init__.py
+++ Sensor_Fault_Detection/constants/Training_pipeline/__init__.py
@@ -25,7 +25,7 @@ DATA_INGESTION_COLLECTION_NAME: str = "sensor"
 DATA_INGETION_DIR_NAME: str = "data_ingetion"
 DATA_INGETION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGETION_INGESTED_DIR: str = "ingested"
-DATA_INGETION_TRAIN_TEST_SPLIT_RATION: float = 0.25
+DATA_INGETION_TRAIN_TEST_SPLIT_RATION: float = 0.20
 
 """
 Data validation realted constant
@@ -44,3 +44,12 @@ Data Transformation ralated constant
 DATA_TRANSFORMATION_DIR_NAME = 'data_transformation'
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR = "transformed_object"
+
+"""
+Model Tranier related Constant
+"""
+MODEL_TRAINER_DIR_NAME:str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str = "traned_model"
+MODEL_FILE_NAME:str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float =0.71
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING: float =0.5
